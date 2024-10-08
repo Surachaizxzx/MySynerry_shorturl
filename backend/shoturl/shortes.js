17bf2d33ba690ec43e3b6c24d12c7357d141a0eb
surachai subnalsing <143887257+Surachaizxzx@users.noreply.github.com>
2024-10-08 22:52:51 +0700

    Update shortes.js

--- backend/shoturl/shortes.js
+++ backend/shoturl/shortes.js
@@ -2,10 +2,10 @@ import axios from "axios";
 const { v4: uuidv4 } = require('uuid');
 const shorturl = async (req, res) => {
     const original = req.body.url;
-    const shortId = uuidv4();
-    const shortUrl = `http://shortez.url/${shortId}`;
+    // const shortId = uuidv4();
+    // const shortUrl = `http://shortez.url/${shortId}`;
     res.status(200).json({
-        shortUrl: `${shortUrl}`
+        shortUrl: `${original}`
     });
 }
 module.exports = shorturl;
