d81de03ede4b064bb665b148822c463fd397ce6f
surachai subnalsing <143887257+Surachaizxzx@users.noreply.github.com>
2024-10-08 22:32:23 +0700

    Update db.js

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,16 +1,16 @@
 import { sql } from "@vercel/postgres";
 const db = async (req, res) => {
     const { original, shortUrl } = req.body;
-    console.log(original);
-    console.log(shortUrl);
+    res.status(200);
     // try {
     //     await sql`
     //       INSERT INTO url_shortener (original_url, short_url)
     //       VALUES (${original}, ${shortUrl});
     //     `;
-    res.status(200).json({ message: "URL added successfully" });
+    //     res.status(200).json({ message: "URL added successfully" });
     // } catch (error) {
-    //     res.status(500).json({ error: "Failed to add URL" });
+    //     console.error("Database insert error:", error);
+    //     res.status(500).json({ error: "Failed to add URL", details: error.message });
     // }
 }
 module.exports = db;
