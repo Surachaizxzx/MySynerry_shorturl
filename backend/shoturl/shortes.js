017cb7d2bbc58f8586ee9dcef6ec9332cb4825c9
surachai subnalsing <143887257+Surachaizxzx@users.noreply.github.com>
2024-10-08 23:14:27 +0700

    Update shortes.js

--- backend/shoturl/shortes.js
+++ backend/shoturl/shortes.js
@@ -3,7 +3,7 @@ const { v4: uuidv4 } = require('uuid');
 const shorturl = async (req, res) => {
     const original = req.body.url;
     const shortId = uuidv4();
-    const shortUrl = `http://shortez.url/${shortId}`;
+    const shortUrl = `https://my-synerry-shorturl.vercel.app/${shortId}`;
     try {
         res.status(200).json({ shortUrl: `${shortUrl}` })
     } catch (error) {
