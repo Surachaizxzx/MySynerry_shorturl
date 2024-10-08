c676fe5b7f095fe3ef58a16614a25748cb98edf4
surachai subnalsing <143887257+Surachaizxzx@users.noreply.github.com>
2024-10-08 19:39:06 +0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express()
 const PORT = 5000
+app.use(express.json());
 app.listen(PORT, () => {
     console.log('Listening on PORT 5000')
 })
+//https://my-synerry-shorturl.vercel.app/
 app.post('/api/keep_url', (req, res) => {
     const url = req.body.url
     res.status(200).json({
