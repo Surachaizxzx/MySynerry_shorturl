4c148c1623b76f00026198b462f5d8ba63540e4b
surachai subnalsing <143887257+Surachaizxzx@users.noreply.github.com>
2024-10-08 23:54:30 +0700

    Update db.js

--- backend/database/db.js
+++ backend/database/db.js
@@ -2,10 +2,16 @@ const { sql } = require('@vercel/postgres');
 const db = async (req, res) => {
     const { original, shortUrl } = req.body;
     try {
-        await sql`
+        // ทำการ INSERT และรับค่าจำนวนแถวที่ถูกเปลี่ยนแปลง
+        const result = await sql`
           INSERT INTO url_shortener (original_url, short_url)
-          VALUES (${original}, ${shortUrl});`;
-        res.status(200).json({ message: "URL added successfully" });
+          VALUES (${original}, ${shortUrl}) RETURNING *;`;
+        // ตรวจสอบว่ามีการ INSERT ข้อมูลหรือไม่
+        if (result.count > 0) {
+            return res.status(200).json({ message: "URL added successfully", data: result });
+        } else {
+            return res.status(500).json({ error: "No rows affected" });
+        }
     } catch (error) {
         console.error("Database insert error:", error);
         res.status(500).json({ error: "Failed to add URL", details: error.message });
