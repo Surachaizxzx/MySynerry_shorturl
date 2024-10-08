4e3f608c7395ea28f977d0c82552eeccf969b06b
surachai subnalsing <143887257+Surachaizxzx@users.noreply.github.com>
2024-10-09 01:10:25 +0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,28 +1,29 @@
-const express = require('express')
-const shorturl = require('./shoturl/shortes')
-const redirectToOriginal = require('./shoturl/redirectToOriginal')
-const http = require('http')
+const express = require('express');
+const shorturl = require('./shoturl/shortes');
+const redirectToOriginal = require('./shoturl/redirectToOriginal');
+const http = require('http');
 const cors = require('cors');
-const db = require('./database/db')
+const db = require('./database/db');
+const { sql } = require('@vercel/postgres');
 const app = express();
 const server = http.createServer(app);
-app.use(express.json())
+app.use(express.json());
 app.post('/api/keep_url', (req, res) => {
     return shorturl(req, res);
-})
+});
 app.post('/api/db', (req, res) => {
     return db(req, res);
-})
+});
 app.get('/api/:shortId', async (req, res) => {
     const shortId = req.params.shortId;
     try {
         const result = await sql`
             SELECT original_url FROM url_shortener WHERE short_url = ${shortId};
-        `;
+        `;//เจอ
         if (result.rowCount > 0) {
             const originalUrl = result.rows[0].original_url;
             return res.redirect(originalUrl);
-        } else {
+        } else {//ไม่เจอ
             return res.status(404).json({ error: 'Short URL not found' });
         }
     } catch (error) {
@@ -31,7 +32,7 @@ app.get('/api/:shortId', async (req, res) => {
     }
 });
 const PORT = 5000;
-server.listen(PORT, () => {
-    console.log(`server running on port ${PORT}`)
-});
 
+server.listen(PORT, () => {
+    console.log(`server running on port ${PORT}`);
+});
