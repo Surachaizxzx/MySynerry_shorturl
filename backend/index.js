fc22671a04806a5805a6b3c19a498a2f576a762b
surachai subnalsing <143887257+Surachaizxzx@users.noreply.github.com>
2024-10-09 00:58:34 +0700

    ำ
    ำ

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const shorturl = require('./shoturl/shortes')
+const redirectToOriginal = require('./shoturl/redirectToOriginal')
 const http = require('http')
 const cors = require('cors');
 const db = require('./database/db')
@@ -12,8 +13,8 @@ app.post('/api/keep_url', (req, res) => {
 app.post('/api/db', (req, res) => {
     return db(req, res);
 })
-app.get('/api/:shortId', (req, res) => {
-    console.log("hello world")
+app.get('/api/:', (req, res) => {
+    return redirectToOriginal(req, res);
 })
 const PORT = 5000;
 server.listen(PORT, () => {
--- backend/shoturl/redirectToOriginal.js
+++ backend/shoturl/redirectToOriginal.js
@@ -1,3 +1,18 @@
-const redirectToOriginal = (req, res) => {
-    const shortId = req.params.shortId; // รับ shortId จาก URL params เเบบget
+const redirectToOriginal = async (req, res) => {
+    const shortId = req.params.shortId;
+    try {
+        const result = await sql`
+            SELECT original_url FROM url_shortener WHERE short_url = ${shortId};
+        `;
+        if (result.rowCount > 0) {//มีมั้ย ลิ้งนี้
+            const originalUrl = result.rows[0].original_url;
+            return res.redirect(originalUrl);
+        } else {
+            // ถ้าไม่พบ shortId ให้ส่งข้อความที่ชัดเจน
+            return res.status(404).json({ error: 'Short URL not found' });
+        }
+    } catch (error) {
+        console.error('Database query error:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
 }
