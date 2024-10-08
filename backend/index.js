12b54657732d324b99ec9f9c9e693f5f49d70834
surachai subnalsing <143887257+Surachaizxzx@users.noreply.github.com>
2024-10-08 23:46:37 +0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -12,9 +12,9 @@ app.post('/api/keep_url', (req, res) => {
 app.post('/api/db', (req, res) => {
     return db(req, res);
 })
-app.get('/api/:shortId', (req, res) => {
-    console.log("hello world")
-})
+// app.get('/api/:shortId', (req, res) => {
+//     console.log("hello world")
+// })
 const PORT = 5000;
 server.listen(PORT, () => {
     console.log(`server running on port ${PORT}`)
