fa0f0fd8a73f4e9560cb2f54c0424577b6aa77ea
surachai subnalsing <143887257+Surachaizxzx@users.noreply.github.com>
2024-10-08 22:50:57 +0700

    error 500

--- backend/shoturl/shortes.js
+++ backend/shoturl/shortes.js
@@ -4,11 +4,8 @@ const shorturl = async (req, res) => {
     const original = req.body.url;
     const shortId = uuidv4();
     const shortUrl = `http://shortez.url/${shortId}`;
-    try {
-        const response = await axios.post('https://my-synerry-shorturl.vercel.app/api/db', { original, shortUrl })
-        res.status(200).json({ shortUrl: `${shortUrl}` })
-    } catch (error) {
-        console.log("Fail  insert to db")
-    }
+    res.status(200).json({
+        shortUrl: `${shortUrl}`
+    });
 }
 module.exports = shorturl;
